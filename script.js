72b37a81a05d52e88db67212dd9910f1c0cf4778
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-07-26 16:32:43 -0700

    Add function to operate expressions

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+let leftNum, rightNum;
+const OPERATORS = new Map()
+  .set("+", add)
+  .set("-", subtract)
+  .set("*", multiply)
+  .set("/", divide);
+
+function operate(leftNum, operator, rightNum) {
+  if (typeof leftNum !== typeof rightNum || typeof leftNum !== "number") {
+    return "ERROR, invalid expression";
+  }
+  if (OPERATORS.has(operator)) {
+    return OPERATORS.get(operator)(leftNum, rightNum);
+  }
+  return "ERROR, false operator";
+}
